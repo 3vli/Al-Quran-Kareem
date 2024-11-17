9ee74694b6c3ea8c2ade682f5029be093fe93e9a
3li <127232417+3vli@users.noreply.github.com>
2024-11-17 09:13:06 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const togglePassword = document.querySelector('.fa-eye');
+const passwordInput = document.querySelector('input[type="password"]');
+
+togglePassword.addEventListener('click', function () {
+    // تغيير نوع المدخل بين password و text
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    // تغيير أيقونة العين
+    this.classList.toggle('fa-eye-slash');
+});
